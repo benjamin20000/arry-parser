0569ed28f587445803972b30bc8c8844062b2286
benjamin <2000beni770@gmail.com>
2025-05-12 18:40:09 +0300

    implementing the validition func

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -2,18 +2,47 @@
 
 class ArrayParser
 {
-    
-    static bool ValidateList(string[] inputArray) {}
+
+    //validate
+    //1: their are 3 items in arr
+    //2: the items are numbers not strings
+    //3: the numbers are positive
+    static bool ValidateList(string[] inputArray) 
+    {
+        if (inputArray.Length < 3)
+        {
+            return false;
+        }
+
+        foreach (string item in inputArray)
+        {
+            foreach (char ch in item)
+            {
+                if (!Char.IsDigit(ch))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     static string[] GetInput()
     {
         Console.WriteLine("enter your numbers: ");
         string input = Console.ReadLine();
         return input.Split(' ');
     }
-    static int DisplayMenu() {}
-    static void Implement(int action, int[]  list) {}
-    static void Print(List<int> list) {}
-    static int[]  convertStrsToInts(string[]  strs) {}
+    // static int DisplayMenu() {}
+    // static void Implement(int action, int[]  list) {}
+
+    static void Print(int[] arr)
+    {
+        foreach (var num in arr)
+        {
+            Console.WriteLine(num);
+        }
+    }
+    // static int[]  convertStrsToInts(string[]  strs) {}
     static void play()
     {
         string[] inputArr = GetInput();
@@ -24,12 +53,13 @@ class ArrayParser
         }
         else
         {
-            int[]  secondList = convertStrsToInts(inputArr);
-            int action = DisplayMenu();
-            if (action != 6)
-            {
-                Implement(action, secondList);
-            }
+            Console.WriteLine("good");
+            // int[]  secondList = convertStrsToInts(inputArr);
+            // int action = DisplayMenu();
+            // if (action != 6)
+            // {
+            //     Implement(action, secondList);
+            // }
         }
     }
 
