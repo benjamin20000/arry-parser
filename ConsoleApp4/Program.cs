27555e6a7e6fce536db3ec3452be969c09cf192a
benjamin <2000beni770@gmail.com>
2025-05-12 19:08:17 +0300

    implement display menu func andd add new func for valideit the user chois

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,7 +7,8 @@ class ArrayParser
     //1: their are 3 items in arr
     //2: the items are numbers not strings
     //3: the numbers are positive
-    static bool ValidateList(string[] inputArray) 
+    //4: their is not empty input
+    static bool ValidateInput(string[] inputArray) 
     {
         if (inputArray.Length < 3)
         {
@@ -16,6 +17,10 @@ class ArrayParser
 
         foreach (string item in inputArray)
         {
+            if (item.Length == 0)
+            {
+                return false;
+            }
             foreach (char ch in item)
             {
                 if (!Char.IsDigit(ch))
@@ -32,7 +37,6 @@ class ArrayParser
         string input = Console.ReadLine();
         return input.Split(' ');
     }
-    // static int DisplayMenu() {}
     // static void Implement(int action, int[]  list) {}
 
     static void Print(int[] arr)
@@ -52,10 +56,34 @@ class ArrayParser
         }
         return newArr;
     }
+
+    static string DisplayMenu()
+    {
+        Console.WriteLine(" Welcome to ArrayParser ");
+        Console.WriteLine("type your chosen numbers:");
+        Console.WriteLine("1: Get new input.");
+        Console.WriteLine("2: Display the series in the order it was entered.");
+        Console.WriteLine("3: Display the series in the reversed order it was entered.");
+        Console.WriteLine("4: Display the series in sorted order (from low to high).");
+        Console.WriteLine("5: Display the Max value of the series.");
+        Console.WriteLine("6: Display the Min value of the series.");
+        Console.WriteLine("7: Display the Average of the series.");
+        Console.WriteLine("8: Display the Number of elements in the series.");
+        Console.WriteLine("9: Display the Sum of the series.");
+        Console.WriteLine("10: Display the Sum of the series.");
+        string action = Console.ReadLine();
+        return action;
+    }
+
+    static bool ValidateAction(string action)
+    {
+        return true;
+    }
+
     static void play()
     {
         string[] inputArr = GetInput();
-        bool listValid = ValidateList(inputArr);
+        bool listValid = ValidateInput(inputArr);
         if (!listValid)
         {
             play();
@@ -64,7 +92,10 @@ class ArrayParser
         {
             Console.WriteLine("good");
             int[]  numbersArrey = convertStrsToInts(inputArr);
-            // int action = DisplayMenu();
+            Print(numbersArrey);
+            string action = DisplayMenu();
+            bool actionValid = ValidateAction(action);
+            
             // if (action != 6)
             // {
             //     Implement(action, secondList);
