102a3b139df91789173483268235287bf1c47a74
benjamin <2000beni770@gmail.com>
2025-05-12 17:25:20 +0300

    implement first func

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,12 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 class ArrayParser
-{   
-    static bool ValidateList(List<int> list) {}
-    static List<int> GetInput() {}
+{
+    
+    static bool ValidateList(List<string> list) {}
+    static List<string> GetInput() {}
     static int DisplayMenu() {}
     static void Implement(int action, List<int> list) {}
     static void Print(List<int> list) {}
-    
-    static void Main(string[] args) {}
+    static List<int> convertStrsToInts(List<string> strs) {}
+    static void play()
+    {
+        List<string> firstList = GetInput();
+        bool listValid = ValidateList(firstList);
+        if (!listValid)
+        {
+            play();
+        }
+        else
+        {
+            List<int> secondList = convertStrsToInts(firstList);
+            int action = DisplayMenu();
+            if (action != 6)
+            {
+                Implement(action, secondList);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        play();
+    }
 }
